754f7fbcc434d616991f6c7b77c3055ec7a60e0d
Thomas Zundel <Thomaszundel@hotmail.com>
2021-02-18 21:36:07 -0800

    coomtt

--- RandomCode/MainForm.Designer.cs
+++ RandomCode/MainForm.Designer.cs
@@ -35,10 +35,9 @@ namespace RandomCode
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(306, 183);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(230, 149);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 84);
+            this.button1.Size = new System.Drawing.Size(142, 68);
             this.button1.TabIndex = 0;
             this.button1.Text = "Centered";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,21 +45,22 @@ namespace RandomCode
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(341, 155);
+            this.checkBox1.Location = new System.Drawing.Point(256, 126);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(119, 21);
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Check Me Out";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MainForm_Load);
